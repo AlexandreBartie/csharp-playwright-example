305db43af20359c34e97b37926e55dfc35ed8d26
Alexandre Bartie <bartie.devops@outlook.com>
2023-06-11 22:06:33 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Playwright;
+
+using var pw = await Playwright.CreateAsync();
+await using var browser = await pw.Chromium.LaunchAsync(new() { Headless = false });
+
+var page = await browser.NewPageAsync();
+
+await page.GotoAsync("http://webcode.me");
+await page.ScreenshotAsync(new() { Path = "screenshot.png" });
